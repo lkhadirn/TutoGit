28b23f4137878226922843c962097c71c707ed6e
Lkhadir <lkhadir.mostapha@gmail.com>
2022-03-14 15:24:20 +0100

    cat version added

--- src/Animal.java
+++ src/Animal.java
@@ -1,14 +1,14 @@
 public class Animal {
     public static void main(String[] args) {
         String firstArgument = args[0];
-        if (firstArgument.equalsIgnoreCase("animal"))
-            System.out.println(getGreeting());
+        if (firstArgument.equalsIgnoreCase("cat"))
+            System.out.println(getCatGreeting());
         else System.out.println("hello word");
 
 
     }
 
-    public static String getGreeting() {
-        return "animal !";
+    public static String getCatGreeting() {
+        return "Meow !";
     }
 }
